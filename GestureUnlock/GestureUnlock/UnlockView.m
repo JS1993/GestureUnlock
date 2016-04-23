846dbe1d7a33e8c8dc04eb75b06e0b6d90990032
susu <28025920@qq.com>
2016-04-23 14:15:53 +0800

    完成手势解锁功能

--- GestureUnlock/GestureUnlock/UnlockView.m
+++ GestureUnlock/GestureUnlock/UnlockView.m
@@ -9,18 +9,97 @@
 
 #import "UnlockView.h"
 
+@interface UnlockView ()
+@property(nonatomic,strong)NSMutableArray* seletedBtns;
+@property(nonatomic,assign)CGPoint curP;
+@end
+
 @implementation UnlockView
 
-- (void)drawRect:(CGRect)rect {
+-(NSMutableArray *)seletedBtns{
+    
+    if (_seletedBtns==nil) {
+        _seletedBtns=[NSMutableArray array];
+    }
+    return _seletedBtns;
+}
+
+-(IBAction)pan:(UIPanGestureRecognizer*)pan{
+    
+    _curP=[pan locationInView:self];
+    
+    for (UIButton* btn in self.subviews) {
+        //如果点在按钮内且未被选中，则加入选中数组，并且重新布局
+        if (CGRectContainsPoint(btn.frame, _curP)&&btn.selected==NO) {
+            
+            btn.selected=YES;
+            
+            [self.seletedBtns addObject:btn];
+        }
+    }
+    [self setNeedsDisplay];
+    
+    //手势结束时还原界面（这个手势结束的时候系统也会自动调用drawRect方法）
+    if (pan.state==UIGestureRecognizerStateEnded) {
+        
+        NSMutableString* strM=[NSMutableString string];
+        
+        for (UIButton* btn in self.seletedBtns) {
+            [strM appendFormat:@"%ld",btn.tag];
+            btn.selected=NO;
+        }
+        
+        //可以做密码匹配
+        NSLog(@"密码：%@",strM);
+        //清除选中按钮数组
+        [self.seletedBtns removeAllObjects];
+    }
+}
+
+-(void)awakeFromNib{
     
     //创建按钮
     for (int i=0; i<9; i++) {
         
         UIButton* btn=[UIButton buttonWithType:UIButtonTypeCustom];
         [btn setImage:[UIImage imageNamed:@"gesture_node_normal"] forState:UIControlStateNormal];
-        [btn setImage:[UIImage imageNamed:@"gesture_node_highlighted"] forState:UIControlStateHighlighted];
+        [btn setImage:[UIImage imageNamed:@"gesture_node_highlighted"] forState:UIControlStateSelected];
+        btn.userInteractionEnabled=NO;
+        btn.tag=i;
         [self addSubview:btn];
     }
+    
+}
+
+- (void)drawRect:(CGRect)rect {
+    
+    if (self.seletedBtns.count==0) return;
+    
+    UIBezierPath* path=[UIBezierPath bezierPath];
+    
+    //把所有选中按钮之间连好线
+    for (int i=0; i<self.seletedBtns.count; i++) {
+        UIButton* btn=self.seletedBtns[i];
+        if (i==0) {
+           
+            [path moveToPoint:btn.center];
+            
+        }else{
+            
+            [path addLineToPoint:btn.center];
+            
+        }
+    }
+    
+    [path addLineToPoint:_curP];
+    
+    //设置线头样式
+    path.lineCapStyle=kCGLineCapRound;
+    //设置连接点样式
+    path.lineJoinStyle=kCGLineJoinRound;
+    path.lineWidth=8;
+    [[UIColor greenColor] set];
+    [path stroke];
 }
 
 -(void)layoutSubviews{
